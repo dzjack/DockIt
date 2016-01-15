cd88f758b117f5e588d763911a60e771ed3e683d
drpain <iam@thatguy.co.za>
2016-01-15 20:22:12 +0200

    User permissions adjustment

--- init_php7.sh
+++ init_php7.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+echo "Ensuring that www-data owns /var/www/"
+chown -R 33:33 /var/www
+chmod +x /var/www/composer.phar
+
 
 echo "CREATING CONTAINER (PHP)"
 docker run \
@@ -7,10 +11,11 @@ docker run \
 -i \
 -t \
 --restart always \
+--user www-data \
 --name php7-fpm \
 --net host \
 -v /var/www:/var/www \
 -w /var/www \
 drpain/php-custom:php7 \
-/bin/bash -c "/var/www/composer.phar update && php-fpm"
+/bin/bash -c "./composer.phar update && php-fpm"
 echo
--- start7.sh
+++ start7.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 echo "Starting Development ENV - PHP7"
+chown -R 33:33 /var/www
 chmod +x /var/www/composer.phar
+
 docker start -a php7-fpm
 
